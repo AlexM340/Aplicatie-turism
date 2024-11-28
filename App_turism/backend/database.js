61e2d0e36051ddf295210d9b9c03da8635e67d48
20DPop <156848353+20DPop@users.noreply.github.com>
2024-11-28 19:11:19 +0200

    migrari database

--- App_turism/backend/database.js
+++ App_turism/backend/database.js
@@ -1,12 +1,33 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-//Creaza conexiunea
+// //Creaza conexiunea
 
-const conexiune = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'turism'
-});
+// const conexiune = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'root',
+//     database: 'turism'
+// });
 
-module.exports = conexiune;
+// module.exports = conexiune;
+
+//Facem cu migrare
+
+const {Sequelize} = require('sequelize');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, 'dataBaseSettings.env') });
+
+//Configuram conexiunea
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+    }
+);
+
+module.exports = sequelize;
--- App_turism/backend/index.js
+++ App_turism/backend/index.js
@@ -1,20 +1,33 @@
 // index.js
 
+
+
 const express = require('express');
-const mysql = require('mysql2/promise');
+// const conexiune = require('./database');
 const cors = require('cors'); // Import CORS
+const sequelize = require('./database');
 require('dotenv').config(); // To use environment variables from the .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
 
 // Test route
-app.get('/test', (req, res) => {
-  res.send('Welcome to the Express backend!');
+app.get('/test', async (req, res) => {
+  try {
+    // Testăm conexiunea cu baza de date
+    await sequelize.authenticate();
+    console.log('Conexiune la baza de date reușită!');
+    res.status(200).send('Conexiunea cu baza de date este activă!');
+  } catch (error) {
+    console.error('Eroare la conectarea MySQL:', error.message);
+    res.status(500).send('Eroare la conectarea cu baza de date.');
+  }
 });
 
 
